f5a97178cb3289ced85c896ded9e4b9064792b11
rpguiteras <rpguiteras@users.noreply.github.com>
2025-03-26 14:15:31 -0400

    script to create svg

---
+++ PricesVsQuantitites/create-SVGs.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# mutool from https://github.com/ArtifexSoftware/mupdf
+# compiled windows mupdf from chocolatey
+
+
+mutool convert -o svg/Ps-vs-Qs-MAC-steep.svg Ps-vs-Qs-MAC-steep.pdf
